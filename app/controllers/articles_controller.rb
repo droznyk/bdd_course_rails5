6ce09210ced27352f9d4f3606b93cb2ced00c1f7
droznyk <droznyk@gmail.com>
2018-11-07 16:41:19 +0000

    Implement edit article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :fetch_article, only: %i[show edit update]
+  
   def index
     @articles = Article.all
   end
@@ -20,7 +22,19 @@ class ArticlesController < ApplicationController
   end
   
   def show
-    @article = Article.find(params[:id])
+  end
+  
+  def edit
+  end
+  
+  def update
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to article_path(@article)
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
   end
   
   protected
@@ -35,5 +49,9 @@ class ArticlesController < ApplicationController
   
     def article_params
       params.require(:article).permit(:title, :body)
+    end
+    
+    def fetch_article
+      @article = Article.find(params[:id])
     end
 end
