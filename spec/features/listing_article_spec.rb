932e912723e5268fccf8157f62594783b44401f0
Patryk Stepien <droznyk@gmail.com>
2018-11-26 15:17:47 +0100

    Fix broken listing articles spec

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Listing articles" do
   before do
-    @article1 = Article.create(title: "The first article", body: "First article body")
-    @article2 = Article.create(title: "The second article", body: "Second article body")
+    john = User.create(email: 'john@example.com', password: 'password')
+    @article1 = Article.create(title: "The first article", body: "First article body", user: john)
+    @article2 = Article.create(title: "The second article", body: "Second article body", user: john)
   end
   
   scenario "A user lists all articles" do
