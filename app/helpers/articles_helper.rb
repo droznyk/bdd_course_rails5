6cdbf5a44729f25b603e5a3d91edfdc285a5ff69
Patryk Stepien <droznyk@gmail.com>
2018-11-27 15:28:59 +0100

    Add persisted_comments method to articles helper

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,4 +2,8 @@ module ArticlesHelper
   def article_owner?(article)
     current_user == article.user
   end
+
+  def persisted_comments(comments)
+    comments.reject{ |comment| comment.new_record?}
+  end
 end
